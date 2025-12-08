df0fb6dd05b107f5e3b8acdda9eb860ef452c7d7
maheswar-dileep <maheaswerd@gmail.com>
2025-12-08 21:24:05 +0530

    feat: Enhance SelectInput styling with a custom dropdown icon and focus ring.

--- src/components/atomic/SelectInput.tsx
+++ src/components/atomic/SelectInput.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { twMerge } from 'tailwind-merge';
+import { ChevronDown } from 'lucide-react';
 
 type Props = {
     options: Array<{ value: string; label: string }>;
@@ -11,26 +12,31 @@ type Props = {
 
 const SelectInput = (props: Props) => {
     return (
-        <select
+        <div
             className={twMerge(
-                'w-48 h-12 p-2 px-4 rounded-lg bg-teal-300 text-lg flex justify-center items-center',
+                'relative w-48 h-12 rounded-lg bg-teal-300 text-lg transition-all focus-within:ring-2 focus-within:ring-zinc-900/10',
                 props.className
             )}
-            value={props.value}
-            onChange={props.onChange}
-            defaultValue={props.value ? undefined : ''}
         >
-            {props.placeholder && (
-                <option value="" disabled>
-                    {props.placeholder}
-                </option>
-            )}
-            {props.options.map((option) => (
-                <option key={option.value} value={option.value}>
-                    {option.label}
-                </option>
-            ))}
-        </select>
+            <select
+                className="w-full h-full appearance-none bg-transparent outline-none px-4 pr-10 cursor-pointer text-inherit font-inherit rounded-inherit"
+                value={props.value}
+                onChange={props.onChange}
+                defaultValue={props.value ? undefined : ''}
+            >
+                {props.placeholder && (
+                    <option value="" disabled>
+                        {props.placeholder}
+                    </option>
+                )}
+                {props.options.map((option) => (
+                    <option key={option.value} value={option.value}>
+                        {option.label}
+                    </option>
+                ))}
+            </select>
+            <ChevronDown className="absolute right-4 top-1/2 -translate-y-1/2 text-zinc-600 pointer-events-none size-4" />
+        </div>
     );
 };
 
